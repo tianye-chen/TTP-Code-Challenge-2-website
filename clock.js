62e6ace8ecdbe2923e0c0df13326327c2c231977
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2021-11-01 18:28:31 -0400

    fix clock

--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@ function update(){
     var min = time.getMinutes();
     var sec = time.getSeconds();
 
-    document.getElementById("hour").firstChild.nodeValue = ("0"+(hour <= 12) ? `${hour}` : `${hour-12}`).slice(-2)+" :";
+    document.getElementById("hour").firstChild.nodeValue = ("0"+((hour <= 12) ? `${hour}` : `${hour-12}`)).slice(-2)+" :";
     document.getElementById("min").firstChild.nodeValue = ("0"+min).slice(-2)+" :";
     document.getElementById("sec").firstChild.nodeValue = ("0"+sec).slice(-2);
     document.getElementById("AMPM").firstChild.nodeValue = (hour <= 12) ? "AM" : "PM";
